bc218e45adf0450d612795f446a560b0de5805b2
Andrew <71823699+riezong@users.noreply.github.com>
2025-06-23 18:04:19 +1000

    Add all BST methods

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ const test = new Tree();
 test.buildTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 console.log([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 
-test.prettyPrint(
-  test.buildTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]),
-);
+test.prettyPrint(test.root);
+
+console.error("isBalanced");
+console.log(test.isBalanced());
 
 test.insert(10);
 
@@ -19,3 +20,32 @@ test.deleteItem(67);
 test.prettyPrint(test.root);
 
 console.log(test.find(324));
+
+console.error("levelOrder");
+test.levelOrder((node) => console.log(node.data));
+
+console.error("inOrder");
+test.inOrder((node) => console.log(node.data));
+
+console.error("preOrder");
+test.preOrder((node) => console.log(node.data));
+
+console.error("postOrder");
+test.postOrder((node) => console.log(node.data));
+
+console.error("height");
+test.height(8);
+
+console.error("depth");
+test.depth(10);
+
+console.error("isBalanced");
+console.log(test.isBalanced());
+
+console.error("rebalance");
+test.prettyPrint(test.root);
+test.rebalance();
+test.prettyPrint(test.root);
+console.log(test.isBalanced());
+
+// Tie it all together
